7822486c9bb9b15927ee9e23d164a31315e3ed4f
KownYongJin <chaoskyj@gmail.com>
2026-05-14 21:05:29 +0900

    feat: create repository
    Add User, Product, Category, EventLog repository for JPA

---
+++ src/main/java/com/example/eventlogpipeline/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.eventlogpipeline.repository;
+
+import com.example.eventlogpipeline.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+}
