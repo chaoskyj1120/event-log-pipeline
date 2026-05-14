bc8230c6614029a547c5795ea2844c800ea995ad
KwonYongJin <chaoskyj@gmail.com>
2026-05-14 16:29:24 +0900

    first commit

---
+++ src/main/java/com/example/eventlogpipeline/EventLogPipelineApplication.java
@@ -0,0 +1,14 @@
+package com.example.eventlogpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventLogPipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventLogPipelineApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/example/eventlogpipeline/EventLogPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventlogpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventLogPipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
